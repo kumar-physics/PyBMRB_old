8770f64f8801c54c8df57c86c66dd070c5d1d176
Kumaran Baskaran <kbaskaran2@wisc.edu>
2016-01-19 10:59:04 -0600

    Initial commit

---
+++ PyBMRB/PyBMRB.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 19, 2016
+
+@author: kumaran
+'''
+
+class PyBMRB(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
